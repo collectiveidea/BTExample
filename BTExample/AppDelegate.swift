a5be2b42d62d3ee1c5f408ddc7f838bc7b2950d9
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2019-05-02 13:41:42 -0400

    Initial Commit

---
+++ BTExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BTExample
+//
+//  Created by Chris Rittersdorf on 5/2/19.
+//  Copyright © 2019 Chris Rittersdorf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
